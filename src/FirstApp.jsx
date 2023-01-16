4b3a06d3fbc63ef0c21b65374b938acb92590f3c
gorsitho <gorsitho@gmail.com>
2023-01-15 21:13:22 -0500

    Fragments- Agrupar elementos

---
+++ src/FirstApp.jsx
@@ -0,0 +1,14 @@
+
+
+
+export const FirstApp = () => {
+
+    return (
+        <> {/*  Allows you to group multiple items together as previously done with Fragments */}
+        
+        <h1> Daniel </h1>
+        <h2> Velásquez {1 + 3}</h2>
+        </>
+    );
+
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,14 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {App} from './HelloWorldApp';
+import {FirstApp} from './FirstApp'
 
 
-function App() {
-    
-    return (<h1> Hola mundo </h1>);
-}
 
 ReactDOM.createRoot( document.getElementById('root')).render(
     <React.StrictMode>
         <App/>
+        <FirstApp/>
     </React.StrictMode>
 );
